662218eef311921a5416996b536064dffda08b41
Trần Duy Việt <viettd@nal.vn>
2024-04-03 11:23:55 +0700

    rebase: update-for-build-mode

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -7,7 +7,7 @@ import { HeaderProps } from "../types/common.d";
 import { WrappingComponent } from "./Wrapping";
 import { Header } from "./components/Header";
 
-const socket = io("ws://localhost:3031");
+const socket = io(process.env.VITE_APP_WS ?? 'ws://localhost:3031');
 
 export const DefaultLayout: React.FC<{ children: JSX.Element }> = (props: {
   children: JSX.Element;
